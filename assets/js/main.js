16122d4854f2d7ab5e9255e78bbb2e5b81cabbb1
tkguha <tkguha@gmail.com>
2026-01-24 22:16:46 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("RJIC 2026 website loaded");
+
+  // Inject header
+  fetch("partials/header.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("header").innerHTML = data;
+      setActiveLink();
+    });
+
+  // Inject footer
+  fetch("partials/footer.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("footer").innerHTML = data;
+    });
+
+function setActiveLink() {
+  const links = document.querySelectorAll(".main-nav a");
+  const current = window.location.pathname;
+
+  links.forEach(link => {
+    if (current.includes(link.getAttribute("href"))) {
+      link.classList.add("active");
+    }
+  });
+}
+
 });
